21a712372bf0070bfa8bbb3355336bf34833a416
Isaac Ezer <isaac.ezer@gmail.com>
2022-06-12 21:12:30 -0400

    prettier

--- src/App.js
+++ src/App.js
@@ -1,37 +1,94 @@
-import './App.scss';
+import "./App.scss";
 
-import Background2 from './assets/Background2.png';
-import Logo from './assets/Logo.png';
+import Background2 from "./assets/Background2.png";
+import Logo from "./assets/Logo.png";
 
 function App() {
   return (
     <div className="App">
-      <div id="background"><img src={Background2}/></div>
-      <div id="logo"><img src={Logo}/></div>
+      <div id="background">
+        <img src={Background2} />
+      </div>
+      <div id="logo">
+        <img src={Logo} />
+      </div>
       <div id="wrapper">
         <div id="fineline"></div>
         <div id="box">
-        <div id="BioBody">
+          <div id="BioBody">
+            <p class="style2">I'm a technologist living in New York City.</p>
 
-        <p class="style2">I'm a technologist living in New York City.</p>
+            <p>
+              I work as a consultant at{" "}
+              <b>
+                <a href="http://201-created.com">201 Created</a>
+              </b>{" "}
+              and I love working with companies that are doing interesting
+              things with the web, <br />
+              particularly with{" "}
+              <b>
+                <a href="http://emberjs.com">Ember</a>
+              </b>
+              .
+            </p>
+            <p>
+              I have a{" "}
+              <b>
+                <a href="https://medium.com/@isaacezer">blog about Ember</a>
+              </b>{" "}
+              and I've given several{" "}
+              <b>
+                <a href="https://medium.com/@isaacezer/ember-meetup-talks-e35df4c0db67">
+                  conference and meetup talks
+                </a>
+              </b>
+              .
+            </p>
+            <p>
+              I built a website{" "}
+              <b>
+                <a href="http://splanky.net">splanky.net</a>
+              </b>{" "}
+              to show a social graph of New York's jazz scene.
+            </p>
+            <p>
+              In my free time I take in as much jazz, swing dancing, and improv
+              comedy as I possibly can.
+            </p>
+            <p>
+              I used to live in Tokyo and played piano with a swing band that
+              played at festivals such as the{" "}
+              <a href="http://www.youtube.com/watch?v=yiV7caPXssc&feature=youtu.be">
+                Korean Lindy Hop Championships
+              </a>{" "}
+              and{" "}
+              <a href="http://mechakuchaswing.main.jp/ole_2011/OLE2011.html">
+                Osaka Lindy Exchange.
+              </a>
+            </p>
+            <p>
+              Check out video of me jumping off a{" "}
+              <a href="http://www.youtube.com/watch?v=bAVA8xg6qTE">
+                168m bridge in Nepal!
+              </a>
+            </p>
 
-          <p>
-      I work as a consultant at <b><a href="http://201-created.com">201 Created</a></b> and I love working with companies that are doing interesting things with the web, <br/>particularly with <b><a href="http://emberjs.com">Ember</a></b>.
+            <p>
+              You can find me on{" "}
+              <a href="http://www.twitter.com/isaacezer">Twitter</a>,{" "}
+              <a href="http://github.com/iezer">GitHub</a>, and{" "}
+              <a href="http://www.linkedin.com/in/isaacezer">LinkedIn</a>.
+            </p>
 
-    </p>
-    <p>I have a <b><a href="https://medium.com/@isaacezer">blog about Ember</a></b> and I've given several <b><a href="https://medium.com/@isaacezer/ember-meetup-talks-e35df4c0db67">conference and meetup talks</a></b>.</p>
-    <p>I built a website <b><a href="http://splanky.net">splanky.net</a></b> to show a social graph of New York's jazz scene.</p>
-    <p>In my free time I take in as much jazz, swing dancing, and improv comedy as I possibly can.</p>
-    <p>I used to live in Tokyo and played piano with a swing band that played at festivals such as the <a href="http://www.youtube.com/watch?v=yiV7caPXssc&feature=youtu.be">Korean Lindy Hop Championships</a> and <a href="http://mechakuchaswing.main.jp/ole_2011/OLE2011.html">Osaka Lindy Exchange.</a>
-    </p>
-    <p>Check out video of me jumping off a <a href="http://www.youtube.com/watch?v=bAVA8xg6qTE">168m bridge in Nepal!</a></p>
-
-    <p>You can find me on <a href="http://www.twitter.com/isaacezer">Twitter</a>, <a href="http://github.com/iezer">GitHub</a>, and <a href="http://www.linkedin.com/in/isaacezer">LinkedIn</a>.</p>
-
-    <p>Email: <b><a href="mailto:me@isaacezer.com">me@isaacezer.com</a></b></p>
+            <p>
+              Email:{" "}
+              <b>
+                <a href="mailto:me@isaacezer.com">me@isaacezer.com</a>
+              </b>
+            </p>
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
     </div>
   );
 }
